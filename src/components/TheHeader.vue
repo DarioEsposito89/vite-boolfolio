a4bfa08230ae74408cc92a0717854c99786dfa5c
DarioEsposito89 <darioesposito.work@gmail.com>
2023-10-17 00:26:01 +0200

    creato e impostato i componenti

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="navbar navbar-expand-md shadow-sm">
+            <div class="container p-2">
+                <h2 class="pe-3">Portfolio</h2>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#"></a>
+                        </li>
+                    </ul>
+
+                    <ul class="navbar-nav ml-auto">
+            
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="http://127.0.0.1:8000/login">Login</a>
+                        </li>                      
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+</template>
+
+<style lang="scss" scoped>
+
+</style>

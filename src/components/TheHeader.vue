9d06a0e31befeaf706e1f788774806b2df267bd6
DarioEsposito89 <darioesposito.work@gmail.com>
2023-10-18 22:02:02 +0200

    Contacts

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,13 @@
+<script>
+    export default{
+
+    }
+</script>
+
 <template>
     <nav class="navbar navbar-expand-md shadow-sm">
             <div class="container p-2">
-                <h2 class="pe-3">Portfolio</h2>
+                <h2 class="pe-3">PORTFOLIO</h2>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -9,18 +15,19 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     
-                    <ul class="navbar-nav me-auto">
+                    <ul class="navbar-nav me-auto d-flex gap-2">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Home</a>
+                            <router-link class="text-decoration-none" :to="{name: 'homepage'}">Home</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="text-decoration-none" :to="{name: 'contacts'}">Contacts</router-link>
                         </li>
                     </ul>
 
                     <ul class="navbar-nav ml-auto">
-            
                         <li class="nav-item">
                             <a class="nav-link" href="http://127.0.0.1:8000/login">Login</a>
                         </li>                      
-                        
                     </ul>
                 </div>
             </div>
